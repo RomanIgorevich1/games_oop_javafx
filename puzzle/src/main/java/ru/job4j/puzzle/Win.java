a034bfe1f783a90115dd6778fe602c6fe0827af4
RomanIgorevich1 <votmoyapochta1@bk.ru>
2021-11-22 12:36:54 +0300

    Сокобан. Тестовое задание. [#75283 #231456]

--- puzzle/src/main/java/ru/job4j/puzzle/Win.java
+++ puzzle/src/main/java/ru/job4j/puzzle/Win.java
@@ -34,4 +34,3 @@ public class Win {
         return rsl;
     }
 }
-
--- puzzle/src/test/java/ru/job4j/puzzle/WinTest.java
+++ puzzle/src/test/java/ru/job4j/puzzle/WinTest.java
@@ -5,8 +5,6 @@ import org.junit.Test;
 import static org.hamcrest.core.Is.is;
 import static org.junit.Assert.*;
 
-
-@SuppressWarnings("ALL")
 public class WinTest {
     @Test
     public void whenVerticalWin() {
