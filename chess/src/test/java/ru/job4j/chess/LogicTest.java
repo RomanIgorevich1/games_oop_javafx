8468bc599e8b50850b54150b9b7866bcbbf5a1bf
votmo <votmoyapochta1@bk.ru>
2022-02-09 14:12:26 +0300

    Каркас шахматной доски [#792 #262421]

--- chess/src/test/java/ru/job4j/chess/LogicTest.java
+++ chess/src/test/java/ru/job4j/chess/LogicTest.java
@@ -23,14 +23,16 @@ public class LogicTest {
     }
 
     @Test(expected = FigureNotFoundException.class)
-    public void whenFigureNotFoundException() throws OccupiedCellException, FigureNotFoundException {
+    public void whenFigureNotFoundException() throws OccupiedCellException,
+            FigureNotFoundException {
         Logic logic = new Logic();
         logic.add(new BishopBlack(Cell.G5));
         logic.move(Cell.C1, Cell.G5);
     }
 
     @Test(expected = ImpossibleMoveException.class)
-    public void whenImpossibleMoveException() throws OccupiedCellException, FigureNotFoundException {
+    public void whenImpossibleMoveException() throws OccupiedCellException,
+            FigureNotFoundException {
         Logic logic = new Logic();
         logic.add(new BishopBlack(Cell.C1));
         logic.move(Cell.C1, Cell.C8);
